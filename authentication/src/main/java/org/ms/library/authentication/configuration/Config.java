62c880d7ee4808f48cdb1e3119ab238cce2e4301
luangomes <luangomespctu@gmail.com>
2025-06-15 18:19:06 -0300

    feat: add Spring Cloud API for routing and load balancing

---
+++ authentication/src/main/java/org/ms/library/authentication/configuration/Config.java
@@ -0,0 +1,10 @@
+package org.ms.library.authentication.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+
+}
---
+++ authentication/src/test/java/org/ms/library/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ms.library.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
