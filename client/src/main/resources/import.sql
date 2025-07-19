1f30092d86efa678048e4c3769c3af28d2f8adb6
luangomes <luangomespctu@gmail.com>
2025-07-19 18:02:21 -0300

    Revert "fix: change service name, create relationship between import file from client and user applications."
    This reverts commit 489454a6866450c3ccdbb53b04642f8cd728ce89.

--- client/src/main/resources/import.sql
+++ client/src/main/resources/import.sql
@@ -2,8 +2,8 @@ INSERT INTO tb_address (address, city, state, zip, country) VALUES ('13th Street
 INSERT INTO tb_address (address, city, state, zip, country) VALUES ('20 Cooper Square', 'New York','NY', 10003, 'USA');
 INSERT INTO tb_address (address, city, state, zip, country) VALUES ('2nd Street Dorm', 'New York','NY', 10004, 'USA');
 
-INSERT INTO tb_client (name, last_name, cpf, phone, address_id, user_id) VALUES ('John', 'Doe', '37120873067', '41894947384', 1, '02d3a9de-2620-4f6a-991e-1b306223c584');
-INSERT INTO tb_client (name, last_name, cpf, phone, address_id) VALUES ('Mary', 'Hopkins', '37120873068', '41794947384', 2, 'c460875e-4799-480b-9951-f657964d2cf7');
-INSERT INTO tb_client (name, last_name, cpf, phone, address_id) VALUES ('John', 'Fidoe', '17120873068', '40794947384', 3, '2cd8f6ee-93d9-48e1-a6fd-73362b7b1007');
+INSERT INTO tb_client (name, last_name, cpf, phone, address_id) VALUES ('John', 'Doe', '37120873067', '41894947384', 1);
+INSERT INTO tb_client (name, last_name, cpf, phone, address_id) VALUES ('Mary', 'Hopkins', '37120873068', '41794947384', 2);
+INSERT INTO tb_client (name, last_name, cpf, phone, address_id) VALUES ('John', 'Fidoe', '17120873068', '40794947384', 3);
 
 
--- rental/rental/src/main/java/org/ms/library/rental/feign/ClientFeign.java
+++ rental/rental/src/main/java/org/ms/library/rental/feign/ClientFeign.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient (name = "CLIENT-SERVICE", path = "/api/client")
+@FeignClient (name = "client", path = "/api/client")
 public interface ClientFeign {
 
     @GetMapping("/{id}")
